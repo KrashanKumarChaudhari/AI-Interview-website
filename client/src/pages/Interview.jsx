7a46590f8f217f66e0c0276b9dcf3eb79af4d374
Krashan Kumar <krashankumarchaudhari00@gmail.com>
2026-09-19 05:43:08 +0530

    score

--- client/src/pages/Interview.jsx
+++ client/src/pages/Interview.jsx
@@ -1,39 +1,87 @@
 import { useState } from "react";
 
+const questions = [
+  "Tell me about yourself.",
+  "What are your strengths?",
+  "What are your weaknesses?",
+  "Why should we hire you?",
+  "Where do you see yourself in five years?"
+];
+
 function Interview() {
+  const [answer, setAnswer] = useState("");
+  const [questionNumber, setQuestionNumber] = useState(1);
+  const [completed, setCompleted] = useState(false);
+  const [answers, setAnswers] = useState([]);
 
-    const [answer, setAnswer] = useState("");
-    
-    return (
+  const score = answers.filter((ans) => ans.trim() !== "").length;
+  let performanceMessage = "";
+
+  if (score >= 4) {
+    performanceMessage = "Great job! You performed well.";
+  } else if (score >= 2) {
+    performanceMessage = "Good effor! Keep practicing.";
+  } else {
+    performanceMessage = "Keep practicing and improve your answers.";
+  }
+
+  return (
+    <div>
+      {completed ? (
         <div>
-            <h1>AI Interview</h1>
+          <h1>Interview Completed</h1>
+          <p>Thank you for completing the interview.</p>
+          <h2>Your Score: {score} / {questions.length}</h2>
+          <p>{performanceMessage}</p>
 
-            <h2>Question 1</h2>
+          <h2>Your Answers</h2>
 
-            <p>Tell me about yourself.</p>
+          {
+            answers.map((ans, index) => (
+              <p key={index}>
+                    <strong>Question {index +1}:</strong> {ans}
+              </p>  
 
-<textarea
-  placeholder="Type your answer here..."
-  rows="6"
-  cols="50"
-  value={answer}
-  onChange={(e) => setAnswer(e.target.value)}
-/>
-
-<br />
-<br />
-
-<button 
-    onClick={() => {
-        console.log(answer);
-        setAnswer("");
-    }}
->
-
-  Submit Answer
-</button> 
+            ))}
         </div>
-    );
+      ) : (
+        <div>
+          <h1>AI Interview</h1>
+
+          <h2>Question {questionNumber}</h2>
+
+          <p>{questions[questionNumber - 1]}</p>
+
+          <textarea
+            placeholder="Type your answer here..."
+            rows="6"
+            cols="50"
+            value={answer}
+            onChange={(e) => setAnswer(e.target.value)}
+          />
+
+          <br />
+          <br />
+
+          <button
+            onClick={() => {
+              console.log(answer);
+              setAnswers([...answers, answer])
+              setAnswer("");
+
+              if (questionNumber < questions.length) {
+                setQuestionNumber(questionNumber + 1);
+              } else {
+                setCompleted(true);
+              }
+            }}
+          >
+            Submit Answer
+          </button>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Interview;
